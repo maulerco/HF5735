4bb3a8f4c453935bff800d78eb0a832d0a620549
Ivan Belov <ivbel2281337@gmail.com>
2024-09-09 14:09:35 +0100

    add htaccess

--- musiconline/public/index.php
+++ musiconline/public/index.php
@@ -1,6 +1,6 @@
 <?php
     include "../includes/header.php";
     include "../login/index.php";
-    include "../register/index.php";
+    // include "../register/index.php";
     include "../includes/footer.php";
 ?>
